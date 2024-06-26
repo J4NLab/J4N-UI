9ea65f28b0f471bcb35b85c3b33fdfca77148689
ottovenliu <otto.ven.liu@gmail.com>
2024-06-26 16:24:36 +0800

    feat: Breadcrumb

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState } from "react";
+import Breadcrumb from "./components/Breadcrumb";
 import Button from "./components/Button";
 import {
   Dialog,
@@ -13,8 +14,26 @@ import { colorList } from "./mocks/_colorList";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const crumb = [
+    {
+      iconPosition: "left",
+      label: "Home",
+      onClick: () => console.log("Home clicked"),
+    },
+    {
+      iconPosition: "right",
+      label: "Profile",
+      onClick: () => console.log("Profile clicked"),
+    },
+    {
+      iconPosition: "empty",
+      label: "Settings",
+      onClick: () => console.log("Settings clicked"),
+    },
+  ];
   return (
     <JanUIProvider customColor={colorList}>
+      <Breadcrumb crumbs={crumb} />
       <Dialog isOpen={isOpen} onClose={() => setIsOpen(!isOpen)}>
         <DialogTitle title="123" status="correct">
           我是標題
